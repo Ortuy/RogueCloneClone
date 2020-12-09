fca29da786cfd66131fdc6edd38ef70c6bdf74f4
Ortuy <Ortuy@users.noreply.github.com>
2020-12-09 21:15:54 +0100

    Prototype touchups
    More items, main menu, pause menu, fade-ins and fade-outs, balancing

---
+++ PixelDungeonClone/Assets/Scripts/ItemDropTable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DropTable", menuName = "Drop Table", order = 2)]
+public class ItemDropTable : ScriptableObject
+{
+    public Item[] itemPool;
+    public int[] itemWeightPool;
+}
